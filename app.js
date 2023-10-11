de669a09e0f5b5d93e7005950576b61783241856
Rohit-Yadav-MIT <112745494+Rohit-Yadav-MIT@users.noreply.github.com>
2023-10-11 11:51:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB using your srv URL
-const mongoURI = 'mongodb+srv://yadavr66752:pass12323@cluster0.th7a4ln.mongodb.net/bank-app';
+const mongoURI = 'your_mongo_srv_url';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Create a schema for the customer
@@ -61,47 +61,6 @@ app.post('/customers', async (req, res) => {
     }
 });
 
-// // Transfer Money (Updated with error handling)
-// app.post('/transfer', async (req, res) => {
-//     const senderId = req.body.sender;
-//     const receiverId = req.body.receiver;
-//     const amount = parseFloat(req.body.amount);
-
-//     if (!senderId || !receiverId || isNaN(amount) || amount <= 0) {
-//         return res.status(400).json({ error: 'Invalid transaction data' });
-//     }
-
-//     const sender = await Customer.findById(senderId);
-//     const receiver = await Customer.findById(receiverId);
-
-//     if (!sender || !receiver) {
-//         return res.status(404).json({ error: 'Sender or receiver not found' });
-//     }
-
-//     if (sender.balance < amount) {
-//         return res.status(400).json({ error: 'Insufficient funds' });
-//     }
-
-//     // Update sender and receiver balances
-//     sender.balance -= amount;
-//     receiver.balance += amount;
-
-//     // Save the updated balances to the database
-//     try {
-//         await sender.save();
-//         await receiver.save();
-//         res.json({ message: 'Transaction successful' });
-//     } catch (error) {
-//         return res.status(500).json({ error: 'Transaction failed' });
-//     }
-// });
-
-// Import a library for generating unique IDs
-const { v4: uuidv4 } = require('uuid');
-
-// ...
-
-// Transfer Money
 app.post('/transfer', async (req, res) => {
     const senderId = req.body.sender;
     const receiverId = req.body.receiver;
